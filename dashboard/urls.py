daff14db06d73c4dfec961ecc9f83df61c965755
Ramesh <developer.rameshraj@gmail.com>
2016-11-07 17:37:29 +0530

    Delete urls.py

--- dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from . import views
-
-
-urlpatterns = [
-    url(r'^get-highdoller-data/$', views.getSearchHighDoller, name='getSearchHighDoller'),
-    url(r'^get-detail-records/$', views.getDetailRecords, name='getDetailRecords'),
-]
