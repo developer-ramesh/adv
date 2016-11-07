62fb8015d7a1d359028aeb0c9c608d513c593063
Ramesh <developer.rameshraj@gmail.com>
2016-11-07 17:39:39 +0530

    Delete urls.py

--- messagemanagement/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from . import views
-
-
-urlpatterns = [
-    url(r'^get-message/$', views.messageIndex, name='messageIndex'),
-    url(r'^message-status/$', views.messageStatus, name='messageStatus'),
-    url(r'^edit-message/$', views.message_edit, name='message_edit'),
-]
